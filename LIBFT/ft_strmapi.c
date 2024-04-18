b9cae8a9e5cc482a337a2b423f89f284e8d4cd86
migueldias2 <167337540+migueldias2@users.noreply.github.com>
2024-04-18 12:16:28 +0100

    Add files via upload

---
+++ LIBFT/ft_strmapi.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcarepa- <mcarepa-@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/12 00:18:49 by mcarepa-          #+#    #+#             */
+/*   Updated: 2024/04/12 01:46:18 by mcarepa-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	len;
+	size_t	i;
+	char	*ptr;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	ptr = (char *)malloc((len + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		ptr[i] = f(i, s[i]);
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+char	uppercase(unsigned int index, char c)
+{
+	if (index % 2 == 0)
+	{
+		if (c >= 'a' && c <= 'z')
+			return (c - 32);
+	}
+	return (c);
+}
+int	main()
+{
+	char *result = ft_strmapi("Ola tudo bem?", &uppercase);
+	printf("Result String: %s\n", result);
+}
